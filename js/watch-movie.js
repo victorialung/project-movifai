7594fc6dae0003ba3fca5d6ec4859ab6381e6b6d
Victoria <vtr.lung@outlook.es>
2018-01-18 16:07:05 -0500

    actualizando estilos

---
+++ js/watch-movie.js
@@ -0,0 +1,13 @@
+// Función para nav-side
+// Initialize collapse button
+$(".button-collapse").sideNav();
+// Initialize collapsible (uncomment the line below if you use the dropdown variation)
+//$('.collapsible').collapsible();
+
+$('.button-collapse').sideNav({
+  menuWidth: 300, // Default is 300
+  edge: 'right', // Choose the horizontal origin
+  closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+  draggable: true,
+}
+);
