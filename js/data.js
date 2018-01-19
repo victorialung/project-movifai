85d4a43b7d4a01198373e991aba9f196018e7932
milagrospalma <mnpalma.2@gmail.com>
2018-01-19 11:05:06 -0500

    Añade archivos js y data

---
+++ js/data.js
@@ -0,0 +1,170 @@
+var data = {
+  'animation': [
+    {
+      'Title': 'The Lion King',
+      'Poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BYTYxNGMyZTYtMjE3MS00MzNjLWFjNmYtMDk3N2FmM2JiM2M1XkEyXkFqcGdeQXVyNjY5NDU4NzI@._V1_SX300.jpg',
+      'Plot': 'Lion cub and future king Simba searches for his identity. His eagerness to please others and penchant for testing his boundaries sometimes gets him into trouble.',
+      'Actors': 'Rowan Atkinson, Matthew Broderick, Niketa Calame, Jim Cummings',
+      'Runtime': '88 min',
+      'Year': 1994
+    }
+  ],
+  'action': [
+    {
+      'Title': 'The Avengers',
+      'Poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTk2NTI1MTU4N15BMl5BanBnXkFtZTcwODg0OTY0Nw@@._V1_SX300.jpg',
+      'Plot': 'Earth\'s mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.',
+      'Actors': 'Robert Downey Jr., Chris Evans, Mark Ruffalo, Chris Hemsworth',
+      'Runtime': '143 min',
+      'Year': 2012
+    }
+  ],
+  'drama': [
+    {
+      'Title': 'Black Mirror',
+      'Poster': 'https://images-na.ssl-images-amazon.com/images/M/M…jMyNTk2XkEyXkFqcGdeQXVyMjYwNDA2MDE@._V1_SX300.jpg',
+      'Plot': 'A television anthology series that shows the dark side of life and technology.',
+      'Actors': 'Hannah John-Kamen, Michaela Coel, Beatrice Robertson-Jones, Jade Clarke',
+      'Runtime': '60 min',
+      'Year': 2011
+    },
+    {
+      'Title': 'Stranger Things',
+      'Poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjUwMDgzOTg3Nl5BMl5BanBnXkFtZTgwNTI4MDk5MzI@._V1_SX300.jpg',
+      'Plot': 'When a young boy disappears, his mother, a police chief, and his friends, must confront terrifying forces in order to get him back.',
+      'Actors': 'Winona Ryder, David Harbour, Finn Wolfhard, Millie Bobby Brown',
+      'Runtime': '51 min',
+      'Year': 2016
+    },
+    {
+      'Title': 'Coco before Chanel',
+      'Poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTA0NjYzMzQxNzBeQTJeQWpwZ15BbWU3MDI2MDA0ODI@._V1_SX300.jpg',
+      'Plot': 'The story of Coco Chanel\'s rise from obscure beginnings to the heights of the fashion world.',
+      'Actors': 'Audrey Tautou, Benoît Poelvoorde, Alessandro Nivola, Marie Gillain',
+      'Runtime': '105 min',
+      'Year': 2009
+    },
+    {
+      'Title': 'This is Us',
+      'Poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTY1MzIwMjA5OV5BMl5BanBnXkFtZTgwNDYyOTExMzI@._V1_SX300.jpg',
+      'Plot': 'Follows a unique ensemble, as their paths cross and their life stories interwine, from sharing the same birthday, to so much more than anyone would expect.',
+      'Actors': 'Milo Ventimiglia, Mandy Moore, Sterling K. Brown, Chrissy Metz',
+      'Runtime': '45 min',
+      'Year': 2016
+    },
+    {
+      'Title': 'Coco',
+      'Poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BYjQ5NjM0Y2YtNjZkNC00ZDhkLWJjMWItN2QyNzFkMDE3ZjAxXkEyXkFqcGdeQXVyODIxMzk5NjA@._V1_SX300.jpg',
+      'Plot': 'Aspiring musician Miguel, confronted with his family\'s ancestral ban on music, enters the Land of the Dead to find his great-great-grandfather, a legendary singer.',
+      'Actors': 'Anthony Gonzalez, Gael García Bernal, Benjamin Bratt, Alanna Ubach',
+      'Runtime': '105 min',
+      'Year': 2017
+    },
+    {
+      'Title': 'Lady Bird',
+      'Poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjg1NDY0NDYzMV5BMl5BanBnXkFtZTgwNzIwMTEwNDI@._V1_SX300.jpg',
+      'Plot': 'In 2002, an artistically inclined seventeen-year-old girl comes of age in Sacramento, California.',
+      'Actors': 'Saoirse Ronan, Laurie Metcalf, Tracy Letts, Lucas Hedges',
+      'Runtime': '94 min',
+      'Year': 2017
+    },
+    {
+      'Title': 'Peaky Blinders',
+      'Poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTQxODYzNTQzOV5BMl5BanBnXkFtZTgwMTI2MDYwMDE@._V1_SX300.jpg',
+      'Plot': 'A gangster family epic set in 1919 Birmingham, England and centered on a gang who sew razor blades in the peaks of their caps, and their fierce boss Tommy Shelby, who means to move up in the world.',
+      'Actors': 'Cillian Murphy, Paul Anderson, Helen McCrory, Sophie Rundle',
+      'Runtime': '60 min',
+      'Year': 2013
+    },
+    {
+      'Title': 'Big Little Lies',
+      'Poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BN2VjYmU2NjYtMzgyZi00YmYwLWJhNWUtNTk4ZmRmNWNmZTljL2ltYWdlXkEyXkFqcGdeQXVyNDkzNTM2ODg@._V1_SX300.jpg',
+      'Plot': 'The apparently perfect lives of three mothers of first graders unravel to the point of murder.',
+      'Actors': 'Reese Witherspoon, Nicole Kidman, Laura Dern, Shailene Woodley',
+      'Runtime': '60 min',
+      'Year': 2017
+    },
+    {
+      'Title': 'The Crown',
+      'Poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BN2VjYmU2NjYtMzgyZi00YmYwLWJhNWUtNTk4ZmRmNWNmZTljL2ltYWdlXkEyXkFqcGdeQXVyNDkzNTM2ODg@._V1_SX300.jpg',
+      'Plot': 'Follows the political rivalries and romance of Queen Elizabeth II\'s reign and the events that shaped the second half of the 20th century.',
+      'Actors': 'Claire Foy, Matt Smith, Victoria Hamilton, Vanessa Kirby',
+      'Runtime': '58 min',
+      'Year': 2016
+    },
+    {
+      'Title': 'The Good Doctor',
+      'Poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTU0Nzk2MDE2MV5BMl5BanBnXkFtZTgwMzg0MzI3MzI@._V1_SX300.jpg',
+      'Plot': 'Shaun Murphy, a young surgeon with autism and Savant syndrome, is recruited into the surgical unit of a prestigious hospital.',
+      'Actors': 'Freddie Highmore, Nicholas Gonzalez, Antonia Thomas, Chuku Modu',
+      'Runtime': '41 min',
+      'Year': 2017
+    },
+    {
+      'Title': 'The Walking Dead',
+      'Poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMzNkMWNmZGUtNWFlZi00MTYwLWIwMjQtOGViN2QzNmI2MWYwXkEyXkFqcGdeQXVyODA1MDc5NjM@._V1_SX300.jpg',
+      'Plot': 'Sheriff Deputy Rick Grimes wakes up from a coma to learn the world is in ruins, and must lead a group of survivors to stay alive.',
+      'Actors': 'Chandler Riggs, Andrew Lincoln, Norman Reedus, Melissa McBride',
+      'Runtime': '44 min',
+      'Year': 2010
+    },
+    {
+      'Title': 'The Room',
+      'Poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTg4MTU1MzgwOV5BMl5BanBnXkFtZTcwNjM1MTAwMQ@@._V1_SX300.jpg',
+      'Plot': 'Johnny is a successful banker who lives happily in a San Francisco townhouse with his fiancée, Lisa. One day, inexplicably, she gets bored with him and decides to seduce his best friend, Mark. From there, nothing will be the same again.',
+      'Actors': 'Tommy Wiseau, Greg Sestero, Juliette Danielle, Philip Haldiman',
+      'Runtime': '99 min',
+      'Year': 2003
+    }
+  ],
+  'comedy': [
+    {
+      'Title': 'Orange Is the New Black',
+      'Poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTU1OTcwNjg2Nl5BMl5BanBnXkFtZTgwOTk0NTk0MjI@._V1_SX300.jpg',
+      'Plot': 'Convicted of a decade old crime of transporting drug money to an ex-girlfriend, normally law abiding Piper Chapman is sentenced to a year and a half behind bars to face the reality of how life changing prison can really be.',
+      'Actors': 'Taylor Schilling, Uzo Aduba, Danielle Brooks, Selenis Leyva',
+      'Runtime': '59 min',
+      'Year': 2013
+    }
+  ],
+  'classic': [
+    {
+      'Title': 'Star Wars: Episode IV - A New Hope',
+      'Poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
+      'Plot': 'Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire\'s world - destroying battle-station while also attempting to rescue Princess Leia from the evil Darth Vader.',
+      'Actors': 'Mark Hamill, Harrison Ford, Carrie Fisher, Peter Cushing',
+      'Runtime': '121 min',
+      'Year': 1977
+    }
+  ],
+  'horror': [
+    {
+      'Title': 'It',
+      'Poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BZDVkZmI0YzAtNzdjYi00ZjhhLWE1ODEtMWMzMWMzNDA0NmQ4XkEyXkFqcGdeQXVyNzYzODM3Mzg@._V1_SX300.jpg',
+      'Plot': 'Convicted of a decade old crime of transporting drug money to an ex-girlfriend, normally law abiding Piper Chapman is sentenced to a year and a half behind bars to face the reality of how life changing prison can really be.',
+      'Actors': 'Jaeden Lieberher, Jeremy Ray Taylor, Sophia Lillis, Finn Wolfhard',
+      'Runtime': '135 min',
+      'Year': 2017
+    }
+  ],
+  'short': [
+    {
+      'Title': 'Dirty Rotten Survival',
+      'Poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTk3OTI1NTYwNl5BMl5BanBnXkFtZTgwOTAwOTExNzE@._V1_SX300.jpg',
+      'Plot': 'Three friends reinvent the great American road trip by giving each other survival challenges. Dick is an engineer, Johnny is a builder and Dave is a primitive survivalist. With each episode...',
+      'Actors': 'Dave Canterbury, John Littlefield, Dick Strawbridge',
+      'Runtime': 'N/A',
+      'Year': 2015
+    }
+  ],
+  'recommended': [
+    {
+      'Title': 'Insidious',
+      'Poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTYyOTAxMDA0OF5BMl5BanBnXkFtZTcwNzgwNTc1NA@@._V1_SX300.jpg',
+      'Plot': 'A family looks to prevent evil spirits from trapping their comatose child in a realm called The Further.',
+      'Actors': 'Patrick Wilson, Rose Byrne, Ty Simpkins, Lin Shaye',
+      'Runtime': '103 min',
+      'Year': 2010
+    }
+  ]
+};
--- js/home.js
+++ js/home.js
@@ -2,31 +2,86 @@
 $(document).ready(function() {
 // activando barra lateral de peril de usuario
   $('.show-user-perfil').sideNav();
-// activando slider
+  // activando slider
   $('.carousel.carousel-slider').carousel({fullWidth: true});
 
-  //Función para
-$(document).ready(function () {
-  $('input.autocomplete').autocomplete({
-    data: {
-      "Apple": null,
-      "Microsoft": null,
-      "Google": null,
-      "Gargle": null
-    }
+  $(document).ready(function() {
+    $('input.autocomplete').autocomplete({
+      data: {
+        'Apple': null,
+        'Microsoft': null,
+        'Google': null,
+        'Gargle': null
+      }
+    });
   });
-});
-// Función para nav-side
-// Initialize collapse button
-$(".button-collapse").sideNav();
-// Initialize collapsible (uncomment the line below if you use the dropdown variation)
-//$('.collapsible').collapsible();
-$('.button-collapse').sideNav({
-  menuWidth: 300, // Default is 300
-  edge: 'right', // Choose the horizontal origin
-  closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-  draggable: true,
-}
-);
+  // Función para nav-side
+  // Initialize collapse button
+  $('.button-collapse').sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  // $('.collapsible').collapsible();
+  $('.button-collapse').sideNav({
+    menuWidth: 300, // Default is 300
+    edge: 'right', // Choose the horizontal origin
+    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    draggable: true,
+  }
+  );
 
+  var arrNumber = [];
+  // Evento para las cajas de home
+  $('.col .container-flex').on('click', function() {
+    var number = $(this).data('box');
+    arrNumber.push(number);
+    for (var i = 0; i < arrNumber.length; i++) {
+      switch (true) {
+      case arrNumber[i] === 0:
+        showMovies('recommended');
+        break;
+      case arrNumber[i] === 1:
+        showMovies('short');
+        break;
+      case arrNumber[i] === 2:
+        showMovies('horror');
+        break;
+      case arrNumber[i] === 3:
+        showMovies('classic');
+        break;
+      case arrNumber[i] === 4:
+        showMovies('comedy');
+        break;
+      case arrNumber[i] === 5 :
+        showMovies('drama');
+        break;
+      case arrNumber[i] === 6:
+        showMovies('action');
+        break;
+      case arrNumber[i] === 7:
+        showMovies('animation');
+        break;
+      }
+    }
+    window.location.ref = 'search-theme.html';
+  });
+
+  // Función para mostrar películas en la siguiente página
+  function showMovies(genre) {
+    var arrGenre = Object.keys(data);
+    for (var i = 0; i < arrGenre.length; i++) {
+      if (arrGenre[i] !== genre) {
+        // alert('Lo sentimos, la siguiente vista está en construcción.');
+      } else {
+        var arrMovies = data[arrGenre[i]];
+        for (var j = 0; j < arrMovies.length; j++) {
+          var objMovie = arrMovies[j];
+          localStorage.title = objMovie.Title;
+          localStorage.poster = objMovie.Poster;
+          localStorage.time = objMovie.Runtime;
+          localStorage.year = objMovie.Year;
+          localStorage.plot = objMovie.Plot;
+          localStorage.actors = objMovie.Actors;
+        }
+      }
+    }
+  }
 });
