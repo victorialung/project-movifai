c376af56a87f8c88c6ed0699565b3cd68945f6b6
SteffAhv <tefavv22@gmail.com>
2018-01-18 22:00:27 -0500

    añade firebase, slider

--- js/slider.js
+++ js/slider.js
@@ -1,31 +1,40 @@
 // Inicializamos el document
 $(document).ready(function(){
   $('.slider').slider();
-
- // Initialize Firebase
- var config = {
-  apiKey: "AIzaSyCGfSH5EIJH4SCCU-9M0hksIwIjVQ5LrNk",
-  authDomain: "movifai-b94a2.firebaseapp.com",
-  databaseURL: "https://movifai-b94a2.firebaseio.com",
-  projectId: "movifai-b94a2",
-  storageBucket: "movifai-b94a2.appspot.com",
-  messagingSenderId: "15279201412"
-};
-firebase.initializeApp(config);
+  
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAEq19iFWUKTSHBQ8J4q-tFx-a3AXjg4dg",
+    authDomain: "movifai-e47d0.firebaseapp.com",
+    databaseURL: "https://movifai-e47d0.firebaseio.com",
+    projectId: "movifai-e47d0",
+    storageBucket: "movifai-e47d0.appspot.com",
+    messagingSenderId: "11379636808"
+  };
+  firebase.initializeApp(config);
 // Indicamos que google sera el proveedor 
 var provider = new firebase.auth.GoogleAuthProvider();
 
-// Pop up autenthication 
-$('#btn-login').click(
-  function signUp() {
-    firebase.auth()
-      .signInWithPopup(provider)
-      .then(function (result) {
-        // window.location.href = 'register.html';
-        console.log(result.user.displayName);
-        console.log(result.user.photoURL);
-        window.location.href = 'home.html';
-      })
-  });
+// Función para guardar automáticamente
+  function save(user) {
+    var usuario = {
+      uid: user.uid,
+      username: user.displayName,
+      usermail: user.email,
+      userphoto: user.photoURL
+    };
+    firebase.database().ref('speakup/' + user.uid).set(usuario);
+  };
 
+  // Evento para el botón Inicia sesión
+  $('#btn-login').on('click', function() {
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      save(result.user);
+      // Guardando en el localstorage
+      window.localStorage.setItem('name', result.user.displayName);
+      var name = window.localStorage.getItem('name');
+      window.localStorage.setItem('foto', result.user.photoURL);
+      var photo = window.localStorage.getItem('foto');
+    });
+  });
 });
