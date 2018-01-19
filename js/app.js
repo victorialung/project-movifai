784f52eacd9b4a9c5d36b6acca8f3c94a928daba
Victoria <vtr.lung@outlook.es>
2018-01-19 10:00:01 -0500

    subiendo vista splash con js

---
+++ js/app.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  setTimeout(function () {
+    window.location.href = 'views/inicio.html';
+  }, 3000);
+});
--- js/search.js
+++ js/search.js
@@ -1,52 +1,82 @@
-// $(document).ready(() => {
-//     $('#searchForm').on('submit', (e) => {
-//         let searchText = $('#searchText').val();
-//         getMovies(searchText);
-//         e.preventDefault();
-//     });
-// });
-
-// function getMovies (searchText) {
-//     axios.get('http://www.omdbapi.com/?apikey=924804a&s='+searchText)
-//         .then((response) => {
-//             console.log(response);
-//             let movies = response.data.Search;
-//             let output = '';
-//             $.each(movies, (index, movie) => {
-//                 output += `
-//                 <div class="well col s6 m3">
-//                     <div class = "center-align">
-//                         <img src ="${movie.Poster}"/>
-//                         <div class="text-tittle">
-//                             <h6 class="white-text">${movie.Title}</h6>
-//                         </div>
-//                         <div class="details">
-//                             <a onclick ="movieSelected('${movie.imdbID}')" class = "waves-effect waves-light btn" href="#">Movie Details</a>
-//                         </div>
-//                     </div>
-//                 </div>
-//                 `;
-//             });
-
-//             $('#movies').html(output);
-//         })
-//         .catch((err) => {
-//             console.log(err);
-//         });
-//     }
-
-//     function movieSelected(id){
-
-//     }
-
-$(document).ready(function () {
-  var containerMovies = $('#movies');
-  var searchForm = $('#searchForm');
-
-  searchForm.submit(function (event) {
-    $('#movies').empty();
-    var inputSearch = $('#search-text').val();
-    console.log(inputSearch);
-
-
+$(document).ready(() => {
+  $('#searchForm').on('submit', (e) => {
+    let searchText = $('#searchText').val();
+    getMovies(searchText);
+    e.preventDefault();
   });
+});
+
+function getMovies(searchText) {
+  axios.get('http://www.omdbapi.com/?apikey=40ecac12&s' + searchText)
+    .then((response) => {
+      console.log(response);
+      let movies = response.data.Search;
+      let output = '';
+      $.each(movies, (index, movie) => {
+        output += `
+          <div class="col s6 m3">
+            <div class="well center-align">
+              <img src="${movie.Poster}">
+              <h5>${movie.Title}</h5>
+              <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+            </div>
+          </div>
+        `;
+      });
+
+      $('#movies').html(output);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+function movieSelected(id) {
+  sessionStorage.setItem('movieId', id);
+  window.location = 'movie.html';
+  return false;
+}
+
+function getMovie() {
+  let movieId = sessionStorage.getItem('movieId');
+
+  axios.get('http://www.omdbapi.com?i=' + movieId)
+    .then((response) => {
+      console.log(response);
+      let movie = response.data;
+
+      let output = `
+        <div class="row">
+          <div class="col-md-4">
+            <img src="${movie.Poster}" class="thumbnail">
+          </div>
+          <div class="col-md-8">
+            <h2>${movie.Title}</h2>
+            <ul class="list-group">
+              <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+              <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+              <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+              <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+              <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+              <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
+              <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+            </ul>
+          </div>
+        </div>
+        <div class="row">
+          <div class="well">
+            <h3>Plot</h3>
+            ${movie.Plot}
+            <hr>
+            <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+            <a href="index.html" class="btn btn-default">Go Back To Search</a>
+          </div>
+        </div>
+      `;
+
+      $('#movie').html(output);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
