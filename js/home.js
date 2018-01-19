5d5f81dc996279be35ab0677e4ffd3f0655f55f3
SteffAhv <tefavv22@gmail.com>
2018-01-19 05:10:37 -0500

    reorganizando archivos

--- js/home.js
+++ js/home.js
@@ -2,183 +2,31 @@
 $(document).ready(function() {
 // activando barra lateral de peril de usuario
   $('.show-user-perfil').sideNav();
+// activando slider
+  $('.carousel.carousel-slider').carousel({fullWidth: true});
 
-// slider 
-function HorizontalScroller(elem) {
-  this.scrollbox = elem; // The scrollers viewable area
-  this.scrollImages = this.scrollbox.find("img");
-  this.leftScrollControl = this.scrollbox.siblings(".left-scroll");
-  this.rightScrollControl = this.scrollbox.siblings(".right-scroll");
-
-  // left and right controls
-  // when at scroll extremes
-  this.scrollbox.on("scroll", this.evaluateControlVisibility.bind(this));
-};
-
-HorizontalScroller.prototype = {
-  
-  scrollboxWidth: function() {
-    return this.scrollbox.outerWidth(true);
-  },
-  currentScrollPosition: function() {
-    return this.scrollbox.scrollLeft();
-  },
-  currentRightPosition: function() {
-    return this.currentScrollPosition() + this.scrollboxWidth() - this.totalWidths();
-  },
-  // Maps the image width of each image in the scroller
-  imageWidths: function() {
-    return $.map(this.scrollImages, function(img) { 
-      return $(img).outerWidth(true);
-    })
-  },
-  totalWidths: function() {
-    return this.imageWidths().reduce(function(a,b) { return a+b});
-  },
-  avgWidth: function() {
-    return this.totalWidths() / this.imageWidths().length;
-  },
-  // Determines the number of images in view area.
-  // Number of images changes with responsive CSS
-  imagesAcross: function() {
-    return Math.round( this.scrollboxWidth() / this.avgWidth() );
-  },
-  imageOffsets: function() {
-    return $.map(this.scrollImages, function(img) { 
-      return Math.round($(img).position().left);
-    }); 
-  },
-  indexOfFirst: function(array, value) {
-    value = value || 0;
-    var firstIndex;
-    var i = 0;
-    while (firstIndex === undefined && array.length > i) {
-      if (array[i] >= value)
-        firstIndex = i; 
-      i += 1;
+  //Función para
+$(document).ready(function () {
+  $('input.autocomplete').autocomplete({
+    data: {
+      "Apple": null,
+      "Microsoft": null,
+      "Google": null,
+      "Gargle": null
     }
-    return firstIndex; 
-  },
-  firstVisibleImageIndex: function() {
-    return this.indexOfFirst(this.imageOffsets());
-  },
-  firstVisibleImage: function() {
-    return this.scrollImages[this.firstVisibleImageIndex()];
-  },
-  lastVisibleImageIndex: function() {
-    return this.firstVisibleImageIndex() + this.imagesAcross();
-  },
-  lastVisibleImage: function() {
-    return this.scrollImages[this.lastVisibleImageIndex()];
-  },
-  offset: function() {
-    var offset = $(this.firstVisibleImage()).position().left;
-    return Math.round(offset);
-  },
-  
-  nextScrollPosition: function(direction) {
-    var nextScrollPosition = this.currentScrollPosition() + this.offset();
-
-    switch(direction) {
-      case "left":
-        nextScrollPosition -= this.scrollboxWidth();
-        if (($(this.firstVisibleImage()).outerWidth(true) - this.offset()) < 0) {
-          nextScrollPosition -= $(this.firstVisibleImage()).outerWidth(true);
-        }
-        break;
-      case "right":
-        nextScrollPosition += this.scrollboxWidth();
-        if (this.offset() > 0) {
-          nextScrollPosition -= $(this.firstVisibleImage()).outerWidth(true);
-        }
-        break;
-    }
-    return nextScrollPosition;
-  },
-  // Triggers the animation
-  animateScroll: function(direction) {
-    resetFocusedImg();
-    var scroller = this;
-    setTimeout(function() {
-      scroller.scrollbox.animate({
-        scrollLeft: scroller.nextScrollPosition(direction)
-      }, this.scrollboxWidth())
-    }.bind(this), 100);
-  },
-
-  hideScrollControl: function(control) {
-    control.addClass("invisible");
-  },
-  showScrollControl: function(control) {
-    control.removeClass("invisible");
-  },
-  scrollControlVisibility: function(control) {
-    return control.hasClass("invisible");
-  },
-  scrollAtZero: function() {
-    return this.currentScrollPosition() == 0;
-  },
-  scrollAtMax: function() {
-    return this.currentRightPosition() >= -1;
-  },
-
-  evaluateControlVisibility: function() {
-    var left = this.leftScrollControl;
-    var right = this.rightScrollControl;
-    var leftIsInvisible = this.scrollControlVisibility(left);
-    var rightIsInvisible = this.scrollControlVisibility(right);
-
-    if (this.scrollAtZero()) this.hideScrollControl(left);
-    if (this.scrollAtMax()) this.hideScrollControl(right);
-    if (!this.scrollAtZero() && leftIsInvisible) this.showScrollControl(left);
-    if (!this.scrollAtMax() && rightIsInvisible) this.showScrollControl(right);
-  }
-};
-
-var scrollers = {};
-
-// Detecta scroll en el DOM
-function detectScrollers() {
-  return $.map($(".horiz-scroll"), function(scroller) {
-    return $(scroller).attr("id");
   });
+});
+// Función para nav-side
+// Initialize collapse button
+$(".button-collapse").sideNav();
+// Initialize collapsible (uncomment the line below if you use the dropdown variation)
+//$('.collapsible').collapsible();
+$('.button-collapse').sideNav({
+  menuWidth: 300, // Default is 300
+  edge: 'right', // Choose the horizontal origin
+  closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+  draggable: true,
 }
-function mapScrollers(scrollerIds) {
-  scrollerIds.forEach(function(elem, i , arr) {
-    var scroller = "#" + elem + " .scroll-images";
-    scrollers[elem] = new HorizontalScroller( $(scroller) );
-  });
-}
-function getScrollDirection(button) {
-  return (button.hasClass("left-scroll")) ? "left" : "right"
-}
-function triggerAnimation(button) {
-  var scrollId = button.closest(".horiz-scroll").attr("id");
-  var scrollDirection = getScrollDirection(button);
-  scrollers[scrollId].animateScroll(scrollDirection);
-}
-function listenForScroll() {
-  $(".left-scroll, .right-scroll").on("click", function() {
-    var button = $(this);
-    triggerAnimation(button);
-  });
-}
-function resetFocusedImg() {
-  $(".focused").removeClass("focused");
-}
-// var horizontalScrollImg = $(".horiz-scroll .scroll-images img");
-// horizontalScrollImg.on("click", function() {
-//   if (!$(this).hasClass("focused"))
-//     resetFocusedImg();
-//   $(this).toggleClass("focused");
-// });
+);
 
-// Inicia eventos escuchas 
-function scrollerInit() {
-  var scrollerIds = detectScrollers();
-  mapScrollers(scrollerIds);
-  listenForScroll();
-}
-
-scrollerInit();
 });
---
+++ js/search.js
@@ -0,0 +1,23 @@
+//Función para
+$(document).ready(function () {
+    $('input.autocomplete').autocomplete({
+      data: {
+        "Apple": null,
+        "Microsoft": null,
+        "Google": null,
+        "Gargle": null
+      }
+    });
+  });
+  // Función para nav-side
+  // Initialize collapse button
+  $(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  //$('.collapsible').collapsible();
+  $('.button-collapse').sideNav({
+    menuWidth: 300, // Default is 300
+    edge: 'right', // Choose the horizontal origin
+    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    draggable: true,
+  }
+  );
