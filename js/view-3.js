b3f8d1be92a151d32f65e7361c33033a3193eb17
Isabel <ycnia_1403@hotmail.com>
2018-01-19 12:19:02 -0500

    Actualizando vista del boton movie details

--- js/view-3.js
+++ js/view-3.js
@@ -50,32 +50,37 @@ function getMovies (searchText) {
                 let movie = response.data;
 
                 let output = `
-            <div class="row">
-            <div class="col-md-4">
-                <img src="${movie.Poster}" class="thumbnail">
-            </div>
-            <div class="col-md-8">
-                <h2>${movie.Title}</h2>
-                <ul class="list-group">
-                <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
-                <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
-                <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
-                <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
-                <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
-                <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
-                <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
-                </ul>
-            </div>
-            </div>
-            <div class="row">
-            <div class="well">
-                <h3>Plot</h3>
-                ${movie.Plot}
-                <hr>
-                <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
-                <a href="index.html" class="btn btn-default">Go Back To Search</a>
-            </div>
-            </div>
+                <div class="view-movie black">
+                <div class="row">
+                <br><br>
+                    <div class="col s12 m4 center">
+                        <img src="${movie.Poster}" class="thumbnail">
+                    </div>
+                    <div class="col s12 m8 center white-text">
+                        <h3>${movie.Title}</h3>
+                        <ul class="list-group">
+                        <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+                        <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+                        <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+                        <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+                        <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+                        <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
+                        <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+                        </ul>
+                        <div class="row">
+                            <div class="well">
+                                <h4>Plot</h4>
+                                ${movie.Plot}
+                                <hr>
+                                <br><br>
+                                <div class="buttons-movie center">
+                                <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary active-color">View IMDB</a>
+                                <a href="index.html" class="btn btn-default center active-color">Go Back To Search</a>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
         `;
 
                 $('#movie').html(output);
